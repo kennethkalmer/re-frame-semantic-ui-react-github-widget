fccb5a861492f746af5fb4a603337da14b1040e1
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2017-02-11 18:14:13 +0200

    feat: bring in some basic components from semantic-ui-react

--- src/cljs/re_frame_semantic_ui_react_github_widget/views.cljs
+++ src/cljs/re_frame_semantic_ui_react_github_widget/views.cljs
@@ -3,6 +3,25 @@
             [cljsjs.semantic-ui-react]
             [goog.object]))
 
+;; Easy handle to the top-level extern for semantic-ui-react
+(def semantic-ui js/semanticUIReact)
+
+(defn component
+  "Get a component from sematic-ui-react:
+
+    (component \"Button\")
+    (component \"Menu\" \"Item\")"
+  [k & ks]
+  (if (seq ks)
+    (apply goog.object/getValueByKeys semantic-ui k ks)
+    (goog.object/get semantic-ui k)))
+
+;; Common references
+(def container (component "Container"))
+(def segment (component "Segment"))
+(def dimmer (component "Dimmer"))
+(def loader (component "Loader"))
+(def message (component "Message"))
 (defn main-panel []
   (let [name (re-frame/subscribe [:name])]
     (fn []
